3fd58037da33134d28552419f79677b09ad333f0
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2026-09-16 17:56:26 +0530

    chore: bump version for theme trial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.divyateja.jimvro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.1.11"
+        versionCode = 13
+        versionName = "0.1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
